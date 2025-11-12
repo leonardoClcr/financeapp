f880fad86afdcc0093426ef0b8a8786d8a486722
Leonardo C Rocha <rochalcr.dev@gmail.com>
2025-11-12 19:00:50 -0300

    feat: add UpdateUserUseCase

---
+++ src/use-cases/update-user.js
@@ -0,0 +1,44 @@
+import { EmailAlreadyInUseError } from "../errors/user";
+import { PostgresGetUserByEmailRepository } from "../repositories/postgres/get-user-by-email.js";
+import { PostgresUpdateUserRepository } from "../repositories/postgres/update-user.js";
+import bcrypt from "bcrypt";
+
+export class UpdateUserUseCase {
+  async execute(userId, updateUserParams) {
+    const postgresGetUserByEmailRepository =
+      new PostgresGetUserByEmailRepository();
+
+    // se o e-mail estiver sendo atualizado verificar se ele ja esta em uso
+
+    if (updateUserParams.email) {
+      const userWithProvidedEmail =
+        await postgresGetUserByEmailRepository.execute(updateUserParams.email);
+
+      if (userWithProvidedEmail) {
+        throw new EmailAlreadyInUseError(updateUserParams.email);
+      }
+    }
+
+    const user = {
+      ...updateUserParams,
+    };
+
+    // se a senha estiver sendo atualizada criptografar
+
+    if (updateUserParams.password) {
+      const hashedPassword = await bcrypt.hash(updateUserParams.password, 10);
+
+      user.password = hashedPassword;
+    }
+
+    // chamar o repository para atualizar o usuario
+    const postgresUpdateUserRepository = new PostgresUpdateUserRepository();
+
+    const updatedUser = await postgresUpdateUserRepository.execute(
+      userId,
+      updateUserParams,
+    );
+
+    return updatedUser;
+  }
+}
