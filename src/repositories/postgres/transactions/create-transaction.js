a09f0a8ee2d3eace324e64af1579828d2bddc317
Leonardo C Rocha <rochalcr.dev@gmail.com>
2025-11-24 11:49:24 -0300

    feat: add POstgresCreateTransactionRepository

--- src/repositories/postgres/transactions/create-transaction.js
+++ src/repositories/postgres/transactions/create-transaction.js
@@ -2,7 +2,7 @@ import { PostgresHelper } from "../../../db/postgres/helper";
 
 export class PostgresCreateTransactionRepository {
   async execute(createTransactionParams) {
-    await PostgresHelper.query(
+    const createdTransaction = await PostgresHelper.query(
       "INSERT INTO transactions (id, user_id, name, date, amount, type) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
       [
         createTransactionParams.id,
@@ -13,5 +13,7 @@ export class PostgresCreateTransactionRepository {
         createTransactionParams.type,
       ],
     );
+
+    return createdTransaction[0];
   }
 }
