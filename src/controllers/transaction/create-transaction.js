6507ad10b16d4c74a1003331ee2bc863b8a91304
archlinux <ep.leonardorocha@gmail.com>
2025-11-29 11:07:30 -0300

    feat: add CreateTransactionController

--- src/controllers/transaction/create-transaction.js
+++ src/controllers/transaction/create-transaction.js
@@ -1,7 +1,76 @@
+import { checkIfIdIsValid, invalidIdResponse } from "../helpers";
+import { badRequest, serverError } from "../helpers/http.js";
+import validator from "validator";
+
 export class CreateTransactionController {
   constructor(createTransactionUseCase) {
     this.createTransactionUseCase = createTransactionUseCase;
   }
 
-  async execute() {}
+  async execute(httpRequest) {
+    try {
+      const params = httpRequest.body;
+
+      const requiredFields = [
+        "id",
+        "user_id",
+        "name",
+        "date",
+        "amount",
+        "type",
+      ];
+
+      for (const field of requiredFields) {
+        if (params[field] || params[field].trim().length == 0) {
+          return badRequest({
+            message: `Missing param: ${field}`,
+          });
+        }
+      }
+
+      const userIdIsValid = checkIfIdIsValid(params.user_id);
+
+      if (!userIdIsValid) {
+        return invalidIdResponse();
+      }
+
+      if (params.amount <= 0) {
+        return badRequest({
+          message: "The amount must be greater than 0",
+        });
+      }
+
+      const amountIsValid = validator.isCurrency(params.amount.toString(), {
+        digits_after_decimal: [2],
+        allow_decimal: false,
+        decimal_separator: ".",
+      });
+
+      if (!amountIsValid) {
+        return badRequest({
+          message: "The amount must be a valid currency.",
+        });
+      }
+
+      const type = params.type.trim().toUpperCase();
+
+      const typeIsValid = ["EARNING", "EXPENSE", "INVESTMENT"].includes(type);
+
+      if (typeIsValid) {
+        return badRequest({
+          message: "The type must be EARNING, EXPENSE or INVESTMENT",
+        });
+      }
+
+      const transaction = await this.createTransactionUseCase.execute({
+        ...params,
+        type,
+      });
+
+      return transaction;
+    } catch (error) {
+      console.error(error);
+      return serverError();
+    }
+  }
 }
